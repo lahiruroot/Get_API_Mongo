561733b8355cb275c8f2fa83a465b2a0d145696b
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2021-09-02 18:11:48 +0530

    add

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://lahiru:6ydXrgb8KAKr8Klx@cluster0.7pawp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config')
+
 
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Hello');
-});
+
+// MongoDB Connection
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('MONGOSB connected!'))
+    .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log('Server run at port ${PORT}'))
+app.listen(PORT, () => console.log('Server run at port ${PORT}'))
+
+
+
+
+
 
 // "test": "echo \"Error: no test specified\" && exit 1"
